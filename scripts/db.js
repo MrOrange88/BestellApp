c21fdc3aa476537527a73baa2dd6452b9d92f317
Mr.Orange88 <m.orange@gmx.de>
2026-01-24 12:31:46 +0100

    Add new logo, basket and more style and more content

---
+++ scripts/db.js
@@ -0,0 +1,89 @@
+const steaks = [
+  {
+    name: "Ribeye Steak",
+    price: 29.9,
+    description:
+      "Saftiges Ribeye Steak 350g vom Angus-Rind, perfekt gegrillt und mit Kräuterbutter serviert.",
+    qty: 1,
+  },
+  {
+    name: "Filetsteak",
+    price: 34.5,
+    description: "Zartes Rinderfilet 250g mit Pfeffersauce und Ofenkartoffeln.",
+    qty: 1,
+  },
+  {
+    name: "T-Bone Steak",
+    price: 39.9,
+    description:
+      "Großes 500g T-Bone Steak mit Knochen, serviert mit Grillgemüse.",
+    qty: 1,
+  },
+  {
+    name: "Tomahawk Steak",
+    price: 59.9,
+    description:
+      "Beeindruckendes Tomahawk Steak 900g, langsam gereift und auf dem Grill zubereitet.",
+    qty: 1,
+  },
+  {
+    name: "Rumpsteak",
+    price: 26.9,
+    description:
+      "Klassisches Rumpsteak 300g mit kräftigem Fettrand, serviert mit Steakhouse-Pommes.",
+    qty: 1,
+  },
+];
+const sides = [
+  {
+    name: "Steakhouse Pommes",
+    price: 4.9,
+    description: "Knusprige Pommes frites, klassisch gewürzt.",
+    qty: 1,
+  },
+  {
+    name: "Ofenkartoffel",
+    price: 5.5,
+    description: "Ofenkartoffel mit Sour Cream.",
+    qty: 1,
+  },
+  {
+    name: "Grillgemüse",
+    price: 6.9,
+    description: "Frisch gegrilltes saisonales Gemüse.",
+    qty: 1,
+  },
+  {
+    name: "Coleslaw",
+    price: 3.9,
+    description: "Hausgemachter Krautsalat nach Steakhouse-Art.",
+    qty: 1,
+  },
+];
+
+const drinks = [
+  {
+    name: "Coca-Cola",
+    price: 3.5,
+    description: "Erfrischendes Cola-Getränk 0,3l.",
+    qty: 1,
+  },
+  {
+    name: "Mineralwasser",
+    price: 2.9,
+    description: "Still oder sprudelnd 0,3l.",
+    qty: 1,
+  },
+  {
+    name: "Rotwein Hausmarke",
+    price: 6.5,
+    description: "Kräftiger Rotwein, perfekt zu Steak 0,2l.",
+    qty: 1,
+  },
+  {
+    name: "Bier",
+    price: 4.0,
+    description: "Frisch gezapftes Bier 0,5l.",
+    qty: 1,
+  },
+];
